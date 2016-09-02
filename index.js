f01b36845dba0e8929b6bf751e9ef6c1fea122d9
Hedy Wang <hedywings@gmail.com>
2016-09-02 15:25:36 +0800

    v0.1.0

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 module.exports = {
     gattDefs: {
         service: [
+            { name: 'dInServ', uuid: '0xbb00' },
+            { name: 'aInServ', uuid: '0xbb10' },
             { name: 'keyServ', uuid: '0xbb70' }
+        ],
+        characteristic: [
+            { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
+            { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
